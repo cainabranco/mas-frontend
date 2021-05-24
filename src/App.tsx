a3d23c4d8349aa15727e7a58467a599222df43ce
Gustavo Cainã Branco <cainabranco@gmail.com>
2021-05-23 21:53:40 -0300

    Atividades MAS-Frontend Semana 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
+import GlobalStyles from './styles/global';
+import Routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Frontend My Activities Space</h1>
-    </div>
+    <Router>
+        <Routes/>
+        <GlobalStyles/>
+    </Router>
   );
 }
 
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import { ActivityTable } from "../../components/ActivityTable";
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+
+import {Container} from './styles';
+
+export function Dashboard(){
+    return(
+        <>
+            <Header />
+            <Container>
+                <Summary />
+                <ActivityTable />
+            </Container>
+        </>
+    )
+}
